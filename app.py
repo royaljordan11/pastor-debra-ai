b17986caaa4e968fa4cbc1f060a933ef0a46a781
royaljordan11 <133277887+royaljordan11@users.noreply.github.com>
2025-12-06 01:44:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8109,6 +8109,6 @@ def handle_exception(e):
     return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    # For Flask:
+    port = int(os.environ.get("PORT", "8080"))
     app.run(host="0.0.0.0", port=port)
+
